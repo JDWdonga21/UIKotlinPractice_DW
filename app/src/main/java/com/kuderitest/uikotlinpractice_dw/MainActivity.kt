2f14f7555ea9587a781d20a96b38d30c7d2c8244
jdwdonga21 <ongame21212144@gmail.com>
2022-10-25 20:21:35 +0900

    코틀린에서 기능 구현 버튼 클릭 : setOnClickListener 버튼 클릭시 입력된 내용 get 값 저장 후 저장된 값 set

--- app/src/main/java/com/kuderitest/uikotlinpractice_dw/MainActivity.kt
+++ app/src/main/java/com/kuderitest/uikotlinpractice_dw/MainActivity.kt
@@ -2,10 +2,19 @@ package com.kuderitest.uikotlinpractice_dw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //확인버튼 클릭시 이벤트
+        okBtn.setOnClickListener {
+//            입력된 내용 확인 : contentEdt의 입력된 값 조회(get) 변수에 저장하기
+            val textContent = contentEdt.text.toString()
+//            저장된 값 입력 받음 : 텍스트뷰의 내용으로 반영 (set)
+            resultTxt.text = textContent
+        }
     }
+
 }
